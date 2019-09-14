6bd91d023b294ca05208cd0275371faa1547d5bc
Sean D'Souza <sdammobubbles@gmail.com>
2019-09-14 14:50:39 -0400

    Add temporary App js for dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import AppNavbar from "./components/AppNavbar.jsx";
+import Editor from "./components/Editor.jsx";
+import ImageCard from "./components/ImageCard.jsx";
+import ImagesNoStatus from './components/ImagesNoStatus.jsx';
+import FilenameStatus from './components/ImagesNoStatus.jsx';
 
 function App() {
   return (
     <div className="App">
-      "My App"
+      <AppNavbar />
+      <ImagesNoStatus numberofimages={10} />
+      <ImageCard image={"/bglock.jpg"} label={"tag"} />
+      <Editor />
+      <FilenameStatus filename={"filename"} />
     </div>
   );
 }
