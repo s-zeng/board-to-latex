c02278fedeba8c5c87fb4c0e841866b65abc54c3
Sean D'Souza <sdammobubbles@gmail.com>
2019-10-12 10:55:58 -0400

    Add shell script for running everything

--- app/run2.sh
+++ app/run2.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
- export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/config/credentials.json
- export MATHPIX_CREDENTIALS=$(pwd)/config/mathpix.json
+export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/config/credentials.json
+export MATHPIX_CREDENTIALS=$(pwd)/config/mathpix.json
+#export TEXINPUTS=.:/opt/tex/cur/texmf-dist/tex/latex/latexconfig/
 
- ./terminal.py $1 > output.tex
+./terminal.py $1 > output.tex
 
- exit_status=$?
+exit_status=$?
 
- if [ $exit_status -eq 0 ]; then
-     pdflatex output.tex
-     evince output.pdf
-     # rm output.tex
- fi
+if [ $exit_status -eq 0 ]; then
+    pdflatex output.tex
+    zathura output.pdf
+#    rm output.tex
+fi
