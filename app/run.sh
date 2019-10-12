7aecc90fd31d40f771428ac2204293d0a17bc0e1
Sean D'Souza <sdammobubbles@gmail.com>
2019-10-12 11:12:36 -0400

    Add mathpix env variable to run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/../config/credentials.json
+export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/config/credentials.json
+export MATHPIX_CREDENTIALS=$(pwd)/config/mathpix.json
 python3 app.py
